5cd7cb0cf74898ae28402c61f1477072feb99e4b
dino9611 <aldinorahman36@gmail.com>
2021-10-26 15:34:32 +0700

    day17 sesi2

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -45,12 +45,15 @@ module.exports = {
     }
   },
   getProducts: async (req, res) => {
-    const { pages, limit, priceMin, priceMax, name, categories_id, sort } =
+    let { pages, limit, priceMin, priceMax, name, categories_id, sort } =
       req.query;
     const conn = await mysqldb.promise().getConnection();
+    if (!pages) {
+      pages = 0;
+    }
     let offset = pages * limit; // karena pages dimulai dari 0
     let querySql = "";
-    let sortSql = "";
+    let sortSql = "order by p.id desc";
     if (priceMin) {
       querySql += `and price > ${mysqldb.escape(parseInt(priceMin))} `;
     }
@@ -60,11 +63,14 @@ module.exports = {
     if (name) {
       querySql += `and name like ${mysqldb.escape("%" + name + "%")} `;
     }
-    if (categories_id) {
-      querySql += `and categories_id = ${mysqldb.escape(categories_id)} `;
+
+    if (parseInt(categories_id)) {
+      querySql += `and categories_id = ${mysqldb.escape(
+        parseInt(categories_id)
+      )} `;
     }
     if (sort) {
-      sortSql += `order by ${sort} `;
+      sortSql = `order by ${sort} `;
     }
     try {
       let sql = `select p.id,name,price,image,stock,keterangan,categories_id,category
@@ -85,6 +91,91 @@ module.exports = {
       conn.release();
       console.log("error :", error);
       return res.status(500).send({ message: error.message || "server error" });
+    }
+  },
+  deleteProduct: async (req, res) => {
+    const { id_product } = req.params;
+    const conn = await mysqldb.promise().getConnection();
+    try {
+      let sql = `select id,image from products where id = ?`;
+      let [data] = await conn.query(sql, [id_product]);
+      if (data.length) {
+        sql = `delete from products where id = ?`;
+        await conn.query(sql, [id_product]);
+        if (data[0].image) {
+          // memastikan image ada di path ini dengan existSync
+          if (fs.existsSync("./public" + data[0].image)) {
+            // hapus filenya jika error
+            fs.unlinkSync("./public" + data[0].image);
+          }
+        }
+        conn.release();
+
+        return res.status(200).send({ message: "berhasil delete products" });
+      } else {
+        throw { message: "id tidak ditemukan" };
+      }
+    } catch (error) {
+      conn.release();
+      console.log("error :", error);
+      return res.status(500).send({ message: error.message || "server error" });
+    }
+  },
+  editProduct: async (req, res) => {
+    let path = "/products";
+    const { id_product } = req.params;
+    console.log(req.files);
+    console.log(req.body);
+    const { image } = req.files; // image karena name di fieldnya image
+    // req.body kalo kirim file itu masih json harus kita parse
+    const data = JSON.parse(req.body.data);
+    // imagepath adalah tempat foto disimpan
+    let imagePath = image ? `${path}/${image[0].filename}` : null;
+    // const { name, price, stock, categories_id, keterangan } = data;
+    // add data product to table products
+    const conn = await mysqldb.promise().getConnection();
+    try {
+      let sql = `select id,image from products where id = ?`;
+      let [dataProd] = await conn.query(sql, [id_product]);
+      if (!dataProd.length) {
+        throw { message: "id tidak ditemukan" };
+      }
+      sql = `update products set ? where id = ?`;
+      // let dataUpdate = {
+      //   name: name,
+      //   price,
+      //   categories_id,
+      //   keterangan,
+      //   stock,
+      // };
+      //? ini lebih flexsible
+      let dataUpdate = {
+        ...data,
+      };
+      if (imagePath) {
+        dataUpdate.image = imagePath;
+      }
+      // edit data sql
+      await conn.query(sql, [dataUpdate, id_product]);
+      // jika berhasil edit hapus foto lama
+      if (imagePath) {
+        // jika imagepath-nya ada
+        if (dataProd[0].image) {
+          // jika data image di dataprod tidak null/false
+          // hapus filenya jika error
+          fs.unlinkSync("./public" + dataProd[0].image);
+        }
+      }
+      conn.release();
+      return res.status(200).send({ message: "berhasil edit products" });
+    } catch (err) {
+      if (imagePath) {
+        // hapus filenya jika error
+        fs.unlinkSync("./public" + imagePath);
+      }
+      conn.release();
+      console.log("error :", err);
+      return res.status(500).send({ message: err.message });
     }
   },
 };
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,7 +3,8 @@ const { uploader } = require("../helpers");
 const router = express.Router();
 const { productControllers } = require("./../controllers");
 
-const { addProducts, getProducts } = productControllers;
+const { addProducts, getProducts, deleteProduct, editProduct } =
+  productControllers;
 
 const uploadFileProd = uploader("/products", "PROD").fields([
   { name: "image", maxCount: 3 },
@@ -11,5 +12,7 @@ const uploadFileProd = uploader("/products", "PROD").fields([
 
 router.post("/", uploadFileProd, addProducts);
 router.get("/", getProducts);
+router.delete("/:id_product", deleteProduct);
+router.delete("/:id_product", uploadFileProd, editProduct);
 
 module.exports = router;
